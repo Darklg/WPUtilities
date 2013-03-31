70607701ebaa4cc45baacbdcb5c5ea83772ea0f6
Darklg <kev.rocher@gmail.com>
2013-03-31 18:43:25 +0200

    Fix Query for index & single

--- wp-content/themes/WPUTheme/index.php
+++ wp-content/themes/WPUTheme/index.php
@@ -1,7 +1,6 @@
 <?php
 get_header();
 
-query_posts( );
 if ( have_posts() ) {
     while ( have_posts() ) {
         the_post();
--- wp-content/themes/WPUTheme/single.php
+++ wp-content/themes/WPUTheme/single.php
@@ -3,7 +3,7 @@ get_header();
 the_post();
 ?>
 <article>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
 </article>
 <?php

70a4bc0af685dbd8b7ae5cd1b98e12282f5058ac
Darklg <kev.rocher@gmail.com>
2013-04-01 20:53:17 +0200

    HEAD content in a separate file with essential WordPress functions

--- wp-content/themes/WPUTheme/header.php
+++ wp-content/themes/WPUTheme/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE HTML>
-<html lang="fr-FR">
-<head>
-    <meta charset="UTF-8" />
-    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body>
+<!--[if lt IE 8 ]><html <?php language_attributes(); ?> class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]-->
+<!--[if IE 8 ]><html <?php language_attributes(); ?> class="is_ie8 lt_ie9 lt_ie10"><![endif]-->
+<!--[if IE 9 ]><html <?php language_attributes(); ?> class="is_ie9 lt_ie10"><![endif]-->
+<!--[if gt IE 9]><html <?php language_attributes(); ?> class="is_ie10"><![endif]-->
+<!--[if !IE]><!--> <html <?php language_attributes(); ?>><!--<![endif]-->
+<head><?php include TEMPLATEPATH . '/tpl/header/head.php'; ?></head>
+<body <body <?php body_class(); ?>>
 <header class="main-header">
     <?php include TEMPLATEPATH . '/tpl/header/title.php'; ?>
     <?php include TEMPLATEPATH . '/tpl/header/searchform.php'; ?>
---
+++ wp-content/themes/WPUTheme/tpl/header/head.php
@@ -0,0 +1,4 @@
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+<?php wp_head(); ?>
+<meta name="viewport" content="width=device-width" />

9c317a14ed7e50642a5d9d70f58a2fbaae6235ee
Darklg <kev.rocher@gmail.com>
2013-04-10 23:25:41 +0200

    Category & Tags in metas

--- wp-content/themes/WPUTheme/loop-small.php
+++ wp-content/themes/WPUTheme/loop-small.php
@@ -3,6 +3,8 @@ include dirname( __FILE__ ) . '/z-protect.php';
 ?><article class="loop-small">
     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
     <aside class="lpsm-metas">
+        <?php the_category(', '); ?>
+        •
         <time class="lpsm-time" datetime="<?php echo get_the_time( DATE_W3C ); ?>"><?php echo get_the_time( __( 'F j, Y', 'wputh' ) ); ?></time>
         •
         <?php echo __( 'By', 'wputh' ); ?> <?php echo get_the_author_link(); ?>
--- wp-content/themes/WPUTheme/loop.php
+++ wp-content/themes/WPUTheme/loop.php
@@ -3,9 +3,14 @@ include dirname( __FILE__ ) . '/z-protect.php';
 ?><article class="loop">
     <h2><?php the_title(); ?></h2>
     <aside class="lp-metas">
+        <?php the_category( ', ' ); ?>
+        •
         <time class="lp-time" datetime="<?php echo get_the_time( DATE_W3C ); ?>"><?php echo get_the_time( __( 'F j, Y', 'wputh' ) ); ?></time>
         •
         <?php echo __( 'By', 'wputh' ); ?> <?php echo get_the_author_link(); ?>
     </aside>
     <?php the_content(); ?>
+    <footer>
+        <?php the_tags( '<p><strong>' . __( 'Tags:', 'wputh' ) . '</strong> ', ', ', '</p>' ); ?>
+    </footer>
 </article>

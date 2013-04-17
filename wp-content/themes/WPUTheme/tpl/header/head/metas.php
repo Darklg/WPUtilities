b03f2a91219de54daa2c0fbd6fe1f2cd828253d2
Darklg <kev.rocher@gmail.com>
2013-04-17 22:48:06 +0200

    Open Graph Metas

--- wp-content/themes/WPUTheme/tpl/header/head/metas.php
+++ wp-content/themes/WPUTheme/tpl/header/head/metas.php
@@ -2,18 +2,37 @@
 global $post;
 $metas = array();
 
+$metas['og_sitename'] = array(
+    'property' => 'og:site_name',
+    'content' => htmlentities( get_bloginfo( 'name' ) )
+);
+
+$metas['og_type'] = array(
+    'property' => 'og:type',
+    'content' => 'blog'
+);
+
 if ( is_single() ) {
     // Meta description
     $meta_description = str_replace( array( "\n", "\t", '   ', '  ' ), ' ', trim( strip_tags( $post->post_content ) ) );
     $metas['description'] = array(
+        'name' => 'description',
         'content' => substr( $meta_description, 0, 200 ) . ' ...'
+    );
+    $metas['og_title'] = array(
+        'property' => 'og:title',
+        'content' => get_the_title()
+    );
+    $metas['og_url'] = array(
+        'property' => 'og:url',
+        'content' => get_permalink()
     );
 }
 
-foreach ( $metas as $name => $values ) {
-    echo '<meta name="' . $name . '"';
-    if ( isset( $values['content'] ) ) {
-        echo ' content="' . $values['content'] . '"';
+foreach ( $metas as $values ) {
+    echo '<meta';
+    foreach ( $values as $name => $value ) {
+        echo ' '.$name.'="' . $value . '"';
     }
     echo ' />';
 }

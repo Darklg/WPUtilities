dd87a800905a719f18f7f61de3e8f27b0e858f0b
Darklg <kev.rocher@gmail.com>
2013-07-01 10:45:16 +0000

    Sitemap with homepage & posts

--- wp-content/themes/WPUTheme/page-templates/page-sitemap.php
+++ wp-content/themes/WPUTheme/page-templates/page-sitemap.php
@@ -9,6 +9,10 @@ include dirname( __FILE__ ) . '/../z-protect.php';
 function get_pages_sitemap_child_of( $sitemap_pages = array(), $parent = 0 ) {
     $content = '';
 
+    if ( $parent == 0 ) {
+        $content .= '<li><a href="'.site_url().'">'.__( 'Home page', 'wputh' ).'</a></li>';
+    }
+
     foreach ( $sitemap_pages as $id => $sitemap_page ) {
         if ( $sitemap_page['parent'] == $parent ) {
             $content .= '<li>';
@@ -47,6 +51,11 @@ if ( $wpq_sitemap->have_posts() ) {
 }
 wp_reset_postdata();
 
+$args = array(
+    'posts_per_page' => 100
+);
+$wpq_sitemap_posts = new WP_Query( $args );
+
 /* ----------------------------------------------------------
   Content
 ---------------------------------------------------------- */
@@ -62,8 +71,21 @@ the_post();
     <hr />
     <?php
 if ( !empty( $sitemap_pages ) ) {
+    echo '<h3>'.__( 'Pages', 'wputh' ).'</h3>';
     echo get_pages_sitemap_child_of( $sitemap_pages, 0 );
 }
+
+if ( $wpq_sitemap_posts->have_posts() ) {
+    echo '<h3>'.__( 'Posts', 'wputh' ).'</h3>';
+    echo '<ul>';
+    while ( $wpq_sitemap_posts->have_posts() ) {
+        $wpq_sitemap_posts->the_post();
+        echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+    }
+    echo '</ul>';
+}
+wp_reset_postdata();
+
 ?>
     </div>
 </article>

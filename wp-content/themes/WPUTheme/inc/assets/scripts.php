ce199dd872a70db5846224c7d990ef7e895b6434
Darklg <kev.rocher@gmail.com>
2013-07-07 21:12:22 +0000

    Loading theme JS

--- wp-content/themes/WPUTheme/inc/assets/scripts.php
+++ wp-content/themes/WPUTheme/inc/assets/scripts.php
@@ -1,2 +1,16 @@
 <?php
 include dirname( __FILE__ ) . '/../../z-protect.php';
+
+function wputh_add_javascripts() {
+    $JScripts = array(
+        'mootools' => array(
+            'uri' => '/js/lib/mootools-core-1.4.5-full-nocompat-yc.js'
+        )
+    );
+
+    foreach ( $JScripts as $id => $details ) {
+        wp_register_script( $id, get_template_directory_uri() . $details['uri'] );
+        wp_enqueue_script( $id );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'wputh_add_javascripts' );

0c4d1b757ca49149dd15221b8b593677f038a16f
Darklg <kev.rocher@gmail.com>
2013-07-14 18:19:23 +0200

    Comments example post metas

--- wp-content/themes/WPUTheme/inc/plugins/wputh-post-metas.php
+++ wp-content/themes/WPUTheme/inc/plugins/wputh-post-metas.php
@@ -12,14 +12,10 @@ include dirname( __FILE__ ) . '/../../z-protect.php';
 add_filter( 'wputh_post_metas_boxes', 'set_wputh_post_metas_boxes', 10, 3 );
 
 function set_wputh_post_metas_boxes( $boxes ) {
-    $boxes['box_address'] = array(
-        'name' => 'Box name',
-        'type' => array()
-    );
-    $boxes['box2_address'] = array(
-        'name' => 'Box2 name',
-        'type' => array()
-    );
+    // $boxes['box_address'] = array(
+    //     'name' => 'Box name',
+    //     'type' => array()
+    // );
     return $boxes;
 }
 
@@ -28,13 +24,9 @@ function set_wputh_post_metas_boxes( $boxes ) {
 
 add_filter( 'wputh_post_metas_fields', 'set_wputh_post_metas_fields', 10, 3 );
 function set_wputh_post_metas_fields( $fields ) {
-    $fields['wputh_post_address'] = array(
-        'box' => 'box_address',
-        'name' => 'Address',
-    );
-    $fields['wputh_post_zip'] = array(
-        'box' => 'box2_address',
-        'name' => 'Zip',
-    );
+    // $fields['wputh_post_address'] = array(
+    //     'box' => 'box_address',
+    //     'name' => 'Address',
+    // );
     return $fields;
 }

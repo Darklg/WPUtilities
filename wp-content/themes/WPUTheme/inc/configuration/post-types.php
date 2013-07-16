4a92aa725367e336c932e3bacd7ab4d7753bdaf7
Darklg <kev.rocher@gmail.com>
2013-07-16 22:34:40 +0200

    Default parameters for post types

--- wp-content/themes/WPUTheme/inc/configuration/post-types.php
+++ wp-content/themes/WPUTheme/inc/configuration/post-types.php
@@ -6,14 +6,24 @@ add_action( 'init', 'wputh_add_post_types' );
 function wputh_add_post_types() {
     $post_types = array(
         'task' => array(
-            'name' => __('Task', 'wputh')
+            'name' => __( 'Task', 'wputh' )
         )
     );
 
     foreach ( $post_types as $slug => $post_type ) {
+        // Default label: slug
+        if ( !isset( $post_type['name'] ) ) {
+            $post_type['name'] = ucfirst( $slug );
+        }
+        // Default capabilities
+        if ( !isset( $post_type['supports'] ) ) {
+            $post_type['supports'] = array( 'title', 'editor', 'thumbnail' );
+        }
+
         $args = array(
             'public' => true,
-            'label' => $post_type['name']
+            'label' => $post_type['name'],
+            'supports' => $post_type['supports']
         );
         register_post_type( $slug, $args );
     }

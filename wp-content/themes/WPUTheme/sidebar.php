223a00194d43711e058d6b7e702e08c8824644f9
Darklg <kev.rocher@gmail.com>
2013-07-28 00:53:29 +0200

    Fix sidebar if a widget is too wide

--- wp-content/themes/WPUTheme/sidebar.php
+++ wp-content/themes/WPUTheme/sidebar.php
@@ -2,6 +2,6 @@
 include dirname( __FILE__ ) . '/z-protect.php';
 if(!isset($_POST['ajax'])){
 ?><aside class="main-sidebar">
-    <ul><?php dynamic_sidebar( 'wputh-sidebar' );?></ul>
+    <ul class="main-sidebar--widgets"><?php dynamic_sidebar( 'wputh-sidebar' );?></ul>
 </aside>
 <?php }

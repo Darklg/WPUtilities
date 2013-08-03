a52a06debe415b94b1f8771a9c2a4c6756385c3a
Darklg <kev.rocher@gmail.com>
2013-08-03 23:11:23 +0200

    Default favicon

--- wp-content/themes/WPUTheme/tpl/header/head.php
+++ wp-content/themes/WPUTheme/tpl/header/head.php
@@ -5,6 +5,7 @@ include dirname( __FILE__ ) . '/../../z-protect.php';
 <?php include get_template_directory() . '/tpl/header/head/metas.php'; ?>
 <?php wp_head(); ?>
 <meta name="viewport" content="width=device-width" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" />
 <!--[if lt IE 9]>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/ie/html5.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/ie/selectivizr-min.js"></script>

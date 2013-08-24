ddb5cd50ae4f98fcd37a2ad6da0630c85932a2b1
Darklg <kev.rocher@gmail.com>
2013-08-24 19:17:16 +0200

    CSS Class for main title

--- wp-content/themes/WPUTheme/tpl/header/title.php
+++ wp-content/themes/WPUTheme/tpl/header/title.php
@@ -6,4 +6,4 @@ if(is_home()){
     $main_tag = 'h1';
 }
 
-?><<?php echo $main_tag; ?> class="h1"><a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a></<?php echo $main_tag; ?>>
+?><<?php echo $main_tag; ?> class="h1 main-title"><a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a></<?php echo $main_tag; ?>>

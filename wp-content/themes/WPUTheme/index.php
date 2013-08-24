11e641dafa2bc5ca8230aab1dedc1da49942993f
Darklg <kev.rocher@gmail.com>
2013-08-24 19:18:41 +0200

    Page example without sidebar

--- wp-content/themes/WPUTheme/index.php
+++ wp-content/themes/WPUTheme/index.php
@@ -2,10 +2,9 @@
 include dirname( __FILE__ ) . '/z-protect.php';
 get_header();
 
-echo '<div class="main">';
+echo '<div class="main-content">';
 echo get_the_loop(array(
     'loop' => 'loop-small-thumbnail'
 ));
 echo '</div>';
-get_sidebar();
 get_footer();

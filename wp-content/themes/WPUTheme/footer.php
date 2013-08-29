aa1675b8b235000ebe2c712c94235d252672f3e1
Darklg <kev.rocher@gmail.com>
2013-08-29 22:39:20 +0200

    Footer blocks reorganisation

--- wp-content/themes/WPUTheme/footer.php
+++ wp-content/themes/WPUTheme/footer.php
@@ -1,13 +1,16 @@
-<?php include dirname( __FILE__ ) . '/z-protect.php';
-if(!isset($_POST['ajax'])){ ?>
+<?php
+include dirname( __FILE__ ) . '/z-protect.php';
+if(!isset($_POST['ajax'])){
+?>
 </div>
 <footer class="main-footer">
+    <?php include get_template_directory() . '/tpl/footer/copyright.php'; ?>
+</footer>
 <?php
-include get_template_directory() . '/tpl/footer/copyright.php';
 include get_template_directory() . '/tpl/footer/analytics.php';
 wp_footer();
 ?>
-</footer>
 </body>
 </html>
-<?php }
+<?php
+}

a2081d2f5aaca7a4a7db78786c8eef8bd84e8a05
Darklg <kev.rocher@gmail.com>
2013-09-24 08:49:38 +0200

    Debug Toolbar v 0.4 : Check for login & PHP syntax

--- wp-content/plugins/wpudebugtoolbar/wpudebugtoolbar.php
+++ wp-content/plugins/wpudebugtoolbar/wpudebugtoolbar.php
@@ -2,16 +2,16 @@
 /*
 Plugin Name: WP Utilities Debug toolbar
 Description: Debug toolbar
-Version: 0.3
+Version: 0.4
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 */
 
-if(!is_admin()){
-    add_action('wp_head', 'wputh_debug_display_style');
-    add_action('shutdown', 'wputh_debug_launch_bar', 999);
+if ( !is_admin() ) {
+    add_action( 'wp_head', 'wputh_debug_display_style' );
+    add_action( 'shutdown', 'wputh_debug_launch_bar', 999 );
 }
 
 /* ----------------------------------------------------------
@@ -50,24 +50,27 @@ body {
 ---------------------------------------------------------- */
 
 function wputh_debug_launch_bar() {
-    global $template;
+    global $template, $pagenow;
+    if ( $pagenow == 'wp-login.php' ) {
+        die;
+    }
     echo '<div class="wputh-debug-toolbar">';
     // Theme
     echo 'Theme : <strong>' . wp_get_theme().'</strong>';
     echo ' <em>&bull;</em> ';
     // Template
-    echo 'File : <strong>' . basename($template).'</strong>';
+    echo 'File : <strong>' . basename( $template ).'</strong>';
     echo ' <em>&bull;</em> ';
     // Current language
-    echo 'Lang : <strong>' . get_bloginfo('language').'</strong>';
+    echo 'Lang : <strong>' . get_bloginfo( 'language' ).'</strong>';
     echo ' <em>&bull;</em> ';
     // Memory used
-    echo 'Memory : <strong>'.round(memory_get_peak_usage()/(1024*1024), 3). '</strong> mb';
+    echo 'Memory : <strong>'.round( memory_get_peak_usage()/( 1024*1024 ), 3 ). '</strong> mb';
     echo ' <em>&bull;</em> ';
     // Queries
     echo 'Queries : <strong>' .get_num_queries().'</strong>';
     echo ' <em>&bull;</em> ';
     // Execution time
-    echo 'Time : <strong>' . timer_stop(0).'</strong> sec';
+    echo 'Time : <strong>' . timer_stop( 0 ).'</strong> sec';
     echo '</div>';
 }

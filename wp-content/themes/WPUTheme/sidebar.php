36e293fbf2280e5761321e633d49d61cf03c416b
Darklg <kev.rocher@gmail.com>
2013-10-07 01:00:47 +0200

    Fix conflict for basic ajax request

--- wp-content/themes/WPUTheme/sidebar.php
+++ wp-content/themes/WPUTheme/sidebar.php
@@ -1,6 +1,6 @@
 <?php
 include dirname( __FILE__ ) . '/z-protect.php';
-if(!isset($_POST['ajax'])){
+if(!isset($_POST['is_ajax'])){
 ?><aside class="main-sidebar">
     <ul class="main-sidebar--widgets"><?php dynamic_sidebar( 'wputh-sidebar' );?></ul>
 </aside>

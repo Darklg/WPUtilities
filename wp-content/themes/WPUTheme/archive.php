bcac323f74cc48609b521a270bec92f318e26b82
Darklg <kev.rocher@gmail.com>
2013-10-14 22:32:36 +0200

    Merge wp_title and archive title

--- wp-content/themes/WPUTheme/archive.php
+++ wp-content/themes/WPUTheme/archive.php
@@ -1,10 +1,10 @@
 <?php
 include dirname( __FILE__ ) . '/z-protect.php';
 get_header();
-include get_template_directory() . '/tpl/archive/title.php';
+$shown_title = wputh_get_shown_title();
 echo '<div class="main-content">';
 echo '<h1>'.$shown_title.'</h1>';
-if ( is_year() || is_month() || is_day()) {
+if ( is_year() || is_month() || is_day() ) {
     include get_template_directory() . '/tpl/archive/dates-navigation.php';
 }
 echo get_the_loop();
--- wp-content/themes/WPUTheme/inc/theme/params.php
+++ wp-content/themes/WPUTheme/inc/theme/params.php
@@ -42,7 +42,10 @@ function wputh_wp_title( $title, $sep ) {
     if ( is_singular() ) {
         $new_title = get_the_title();
     }
-    if ( is_tax() || is_tag() || is_category() ) {
+    if ( is_archive() ) {
+        $new_title = wputh_get_shown_title();
+    }
+    if ( is_tax() ) {
         $new_title = single_cat_title( "", false );
     }
 
@@ -50,3 +53,29 @@ function wputh_wp_title( $title, $sep ) {
     return $new_title . $spaced_sep . get_bloginfo( 'name' );
 }
 add_filter( 'wp_title', 'wputh_wp_title', 10, 2 );
+
+
+function wputh_get_shown_title() {
+    global $post;
+    $shown_title = __( 'Archive', 'wputh' );
+    if ( is_tag() ) {
+        $shown_title = __( 'Tag:', 'wputh' ) . ' ' . single_tag_title( "", false );
+    }
+    if ( is_category() ) {
+        $shown_title = __( 'Category:', 'wputh' ) . ' ' . single_cat_title( "", false );
+    }
+    if ( is_author() ) {
+        $curauth = ( isset( $_GET['author_name'] ) ) ? get_user_by( 'slug', $author_name ) : get_userdata( intval( $author ) );
+        $shown_title = __( 'Author:', 'wputh' ) . ' ' . $curauth->nickname;
+    }
+    if ( is_year() ) {
+        $shown_title = __( 'Year:', 'wputh' ) . ' ' . get_the_time( __( 'Y', 'wputh' ) );
+    }
+    if ( is_month() ) {
+        $shown_title = __( 'Month:', 'wputh' ) . ' ' . get_the_time( __( 'F Y', 'wputh' ) );
+    }
+    if ( is_day() ) {
+        $shown_title = __( 'Day:', 'wputh' ) . ' ' . get_the_time( __( 'F j, Y', 'wputh' ) );
+    }
+    return $shown_title;
+}

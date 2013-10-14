9d2d549454d4aafdab9a01b7a232d69b92fead69
Darklg <kev.rocher@gmail.com>
2013-10-14 08:53:18 +0200

    Clearer nav menu.

--- wp-content/themes/WPUTheme/header.php
+++ wp-content/themes/WPUTheme/header.php
@@ -1,10 +1,5 @@
 <?php
 include dirname( __FILE__ ) . '/z-protect.php';
-$menu_args = array(
-    'depth' => 1,
-    'theme_location' => 'main',
-    'menu_class' => 'main-menu'
-);
 if(!isset($_POST['is_ajax'])){
 ?><!DOCTYPE HTML>
 <!--[if lt IE 8 ]><html <?php language_attributes(); ?> class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]-->
@@ -18,7 +13,11 @@ if(!isset($_POST['is_ajax'])){
     <div>
     <?php include get_template_directory() . '/tpl/header/title.php'; ?>
     <?php include get_template_directory() . '/tpl/header/searchform.php'; ?>
-    <?php wp_nav_menu( $menu_args ); ?>
+    <?php wp_nav_menu( array(
+        'depth' => 1,
+        'theme_location' => 'main',
+        'menu_class' => 'main-menu'
+    ) ); ?>
     </div>
 </header>
 <div class="main-container centered-container"><div class="main-container--inner">

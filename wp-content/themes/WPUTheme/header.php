d46d5cc6b7b6567b711260d4be896c2d2915e7c6
Darklg <kev.rocher@gmail.com>
2013-10-17 08:55:16 +0200

    Isolated JS values

--- wp-content/themes/WPUTheme/header.php
+++ wp-content/themes/WPUTheme/header.php
@@ -1,6 +1,6 @@
 <?php
 include dirname( __FILE__ ) . '/z-protect.php';
-if(!isset($_POST['is_ajax'])){
+if ( !isset( $_POST['is_ajax'] ) ) {
 ?><!DOCTYPE HTML>
 <!--[if lt IE 8 ]><html <?php language_attributes(); ?> class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]-->
 <!--[if IE 8 ]><html <?php language_attributes(); ?> class="is_ie8 lt_ie9 lt_ie10"><![endif]-->
@@ -8,22 +8,19 @@ if(!isset($_POST['is_ajax'])){
 <!--[if gt IE 9]><html <?php language_attributes(); ?> class="is_ie10"><![endif]-->
 <!--[if !IE]><!--> <html <?php language_attributes(); ?>><!--<![endif]-->
 <head><?php include get_template_directory() . '/tpl/header/head.php'; ?></head>
-<body <?php body_class('cssc-is-responsive'); ?>>
+<body <?php body_class( 'cssc-is-responsive' ); ?>>
 <header class="main-header centered-container">
     <div>
     <?php include get_template_directory() . '/tpl/header/title.php'; ?>
     <?php include get_template_directory() . '/tpl/header/searchform.php'; ?>
     <?php wp_nav_menu( array(
-        'depth' => 1,
-        'theme_location' => 'main',
-        'menu_class' => 'main-menu'
-    ) ); ?>
+            'depth' => 1,
+            'theme_location' => 'main',
+            'menu_class' => 'main-menu'
+        ) ); ?>
     </div>
 </header>
-<div class="main-container centered-container"><div class="main-container--inner">
-<?php } ?>
-<?php include get_template_directory() . '/tpl/header/breadcrumbs.php'; ?>
-<script>
-var wputh_page_title = "<?php echo addslashes(trim(wp_title('-', 0))); ?>";
-var wputh_pagination_kind = '<?php echo PAGINATION_KIND; ?>';
-</script>
+<div class="main-container centered-container"><div class="main-container--inner" id="content">
+<?php }
+include get_template_directory() . '/tpl/header/breadcrumbs.php';
+include get_template_directory() . '/tpl/header/jsvalues.php';
---
+++ wp-content/themes/WPUTheme/tpl/header/jsvalues.php
@@ -0,0 +1,5 @@
+<script>var wputh_pagination_kind = '<?php echo PAGINATION_KIND; ?>';</script>
+<div id="js-values"
+data-page_title="<?php echo addslashes( trim( get_bloginfo( 'name' ) . wp_title( ' - ', false ) ) ); ?>"
+data-body_<?php body_class( 'cssc-is-responsive' ); ?>
+></div>

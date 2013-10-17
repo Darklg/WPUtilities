2214e69c16fae750334e6bb25fa52734b7b6d2b4
Darklg <kev.rocher@gmail.com>
2013-10-17 13:56:41 +0200

    Disable posts v 0.2 : Remove RSS Feeds

--- wp-content/plugins/wpudisableposts/wpudisableposts.php
+++ wp-content/plugins/wpudisableposts/wpudisableposts.php
@@ -3,12 +3,16 @@
 Plugin Name: WPU disable posts
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: Disable all posts
-Version: 0.1
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 */
+
+/* ----------------------------------------------------------
+  Remove post type "post" support
+---------------------------------------------------------- */
 
 if ( ! function_exists( 'unregister_post_type' ) ) :
     function unregister_post_type( $post_type ) {
@@ -26,7 +30,20 @@ function wputh_disable_posts() {
     unregister_post_type( 'post' );
 }
 
+/* ----------------------------------------------------------
+  Remove edit menu
+---------------------------------------------------------- */
+
 add_action( 'admin_menu', 'wputh_disable_posts_pages' );
 function wputh_disable_posts_pages() {
     remove_menu_page( 'edit.php' );
+}
+
+/* ----------------------------------------------------------
+  Remove Posts and Comments RSS feeds
+---------------------------------------------------------- */
+
+add_action( 'template_redirect', 'wputh_disable_posts_rss_feeds' );
+function wputh_disable_posts_rss_feeds() {
+    remove_action( 'wp_head', 'feed_links', 2 );
 }

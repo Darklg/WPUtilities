510350b1b62b2672858386164310c28faa9a10b5
Darklg <kev.rocher@gmail.com>
2013-11-08 20:26:27 +0100

    Display Instagram v 0.2 : Handle errors & get time

--- wp-content/mu-plugins/wpu_display_instagram.php
+++ wp-content/mu-plugins/wpu_display_instagram.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Display Instagram
 Description: Display Latest Image for Instagram
-Version: 0.1
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -14,20 +14,43 @@ function wpu_get_instagram() {
     $user_id = trim( get_option( 'wpu_get_instagram__user_id' ) );
     $transient_id = 'json_instagram_' . $user_id;
 
+    $return = array(
+        'image' => '',
+        'link' => '#',
+        'created_time' => '0',
+        'caption' => '',
+    );
+
     // Get cached JSON
     $json_instagram = get_transient( $transient_id );
     if ( empty( $json_instagram ) ) {
         $json_instagram = file_get_contents( 'https://api.instagram.com/v1/users/'.$user_id.'/media/recent/?count=1&access_token='. $api_key );
         set_transient( $transient_id, $json_instagram, HOUR_IN_SECONDS );
     }
+
     // Extract and return informations
     $imginsta = json_decode( $json_instagram );
-    $details = $imginsta->data[0];
-    return array(
-        'image' =>  $details->images->standard_resolution->url,
-        'link' =>  $details->link,
-        'caption' =>  $details->caption->text,
-    );
+    if ( isset( $imginsta->data[0] ) ) {
+        $details = $imginsta->data[0];
+        // Image
+        if ( isset( $details->images->standard_resolution->url ) ) {
+            $return['image'] = $details->images->standard_resolution->url;
+        }
+        // Link
+        if ( isset( $details->link ) ) {
+            $return['link'] = $details->link;
+        }
+        // Created time
+        if ( isset( $details->created_time ) ) {
+            $return['created_time'] = $details->created_time;
+        }
+        // Caption
+        if ( isset( $details->caption->text ) ) {
+            $return['caption'] = $details->caption->text;
+        }
+    }
+
+    return $return;
 }
 
 /* ----------------------------------------------------------

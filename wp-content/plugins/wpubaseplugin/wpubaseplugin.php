ae21cd3822fe01bb24814775e955c4468255c5ab
Darklg <kev.rocher@gmail.com>
2013-11-14 00:00:10 +0100

    Base Plugin v 1.5 : Merge utilities & Nonce in admin form

--- wp-content/plugins/wpubaseplugin/wpubaseplugin.php
+++ wp-content/plugins/wpubaseplugin/wpubaseplugin.php
@@ -3,19 +3,14 @@
 Plugin Name: WPU Base Plugin
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: A framework for a WordPress plugin
-Version: 1.4
+Version: 1.5
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 */
 
-// Load Base Plugin Utilities
-if ( !class_exists( 'wpuBasePluginUtilities' ) ) {
-    include dirname( __FILE__ ).'/inc/wpubasepluginutilities.php';
-}
-
-class wpuBasePlugin extends wpuBasePluginUtilities {
+class wpuBasePlugin {
 
     /* ----------------------------------------------------------
       Options
@@ -41,11 +36,6 @@ class wpuBasePlugin extends wpuBasePluginUtilities {
 
     function __construct() {
         $this->set_options();
-        if ( $this->version < 1.0 ) {
-            // Detect outdated version of utilities
-            $error_message = htmlentities( __( 'Error: Your version of %s is outdated!', $this->options['id'] ) );
-            exit(  sprintf( $error_message , '<strong>Base Plugin Utilities</strong>' ) );
-        }
         $this->set_public_hooks();
         if ( is_admin() ) {
             $this->set_admin_hooks();
@@ -100,12 +90,21 @@ class wpuBasePlugin extends wpuBasePluginUtilities {
 
     function set_admin_page_main() {
         echo $this->get_wrapper_start( $this->options['name'] );
+
+        // Content
         echo '<p>'.__( 'Content', $this->options['id'] ).'</p>';
+
+        // Default Form
+        echo '<form action="" method="post"><div>';
+        wp_nonce_field( 'action-main-form', 'action-main-form-'.$this->options['id'] );
+        echo '<button class="button-primary" type="submit">'.__( 'Submit', $this->options['id'] ).'</button>';
+        echo '</div></form>';
+
         echo $this->get_wrapper_end();
     }
 
     function set_admin_page_main_postAction() {
-        if ( empty( $_POST ) ) {
+        if ( empty( $_POST ) || !isset( $_POST['action-main-form-'.$this->options['id']] ) || !wp_verify_nonce( $_POST['action-main-form-'.$this->options['id']], 'action-main-form' ) ) {
             return;
         }
         $this->messages[] = 'Success !';
@@ -173,6 +172,117 @@ class wpuBasePlugin extends wpuBasePluginUtilities {
         global $wpdb;
         $wpdb->query( 'DROP TABLE ' . $this->data_table );
     }
+
+
+    /* ----------------------------------------------------------
+      Utilities : Requests
+    ---------------------------------------------------------- */
+
+    function get_pager_limit( $perpage, $tablename = '' ) {
+        global $wpdb;
+
+        // Ensure good format for table name
+        if ( empty( $tablename ) || !preg_match( '/^([A-Za-z0-9_-]+)$/', $tablename ) ) {
+            return array(
+                'pagenum' => 0,
+                'max_pages' => 0,
+                'limit' => '',
+            );
+        }
+
+        // Ensure good format for perpage
+        if ( empty( $perpage ) || !is_numeric( $perpage ) ) {
+            $perpage = 20;
+        }
+
+        // Get number of elements in table
+        $elements_count = $wpdb->get_var( "SELECT COUNT(*) FROM ".$tablename );
+
+        // Get max page number
+        $max_pages = ceil( $elements_count / $perpage );
+
+        // Obtain Page Number
+        $pagenum = ( isset( $_GET['pagenum'] ) && is_numeric( $_GET['pagenum'] ) ? $_GET['pagenum'] : 1 );
+        $pagenum = min( $pagenum, $max_pages );
+
+        // Set SQL limit
+        $limit = 'LIMIT ' . ( $pagenum * $perpage - $perpage ) . ', '.$perpage;
+
+        return array(
+            'pagenum' => $pagenum,
+            'max_pages' => $max_pages,
+            'limit' => $limit,
+        );
+
+    }
+
+    /* ----------------------------------------------------------
+      Utilities : Export
+    ---------------------------------------------------------- */
+
+    function export_array_to_csv( $array, $name ) {
+        if ( isset( $array[0] ) ) {
+            header( 'Content-Type: application/csv' );
+            header( 'Content-Disposition: attachment; filename=export-list-'.$name.'-'.date( 'y-m-d' ).'.csv' );
+            header( 'Pragma: no-cache' );
+            echo implode( ';', array_keys( $array[0] ) )."\n";
+            foreach ( $array as $line ) {
+                echo implode( ';', $line )."\n";
+            }
+            die;
+        }
+    }
+
+    /* ----------------------------------------------------------
+      Utilities : Display
+    ---------------------------------------------------------- */
+
+    function get_wrapper_start( $title ) {
+        return '<div class="wrap"><div id="icon-options-general" class="icon32"></div><h2 class="title">'.$title.'</h2><br />';
+    }
+
+    function get_wrapper_end() {
+        return '</div>';
+    }
+
+    function get_admin_table( $values, $args = array() ) {
+        $pagination = '';
+        if ( isset( $args['pagenum'], $args['max_pages'] ) ) {
+            $page_links = paginate_links( array(
+                    'base' => add_query_arg( 'pagenum', '%#%' ),
+                    'format' => '',
+                    'prev_text' => '&laquo;',
+                    'next_text' => '&raquo;',
+                    'total' => $args['max_pages'],
+                    'current' => $args['pagenum']
+                ) );
+
+            if ( $page_links ) {
+                $pagination = '<div class="tablenav"><div class="tablenav-pages" style="margin: 1em 0">' . $page_links . '</div></div>';
+            }
+        }
+
+        $content = '<table class="widefat">';
+        if ( isset( $args['columns'] ) && is_array( $args['columns'] ) && !empty( $args['columns'] ) ) {
+            $labels = '<tr><th>' . implode( '</th><th>', $args['columns'] ).'</th></tr>';
+            $content .= '<thead>'.$labels.'</thead>';
+            $content .= '<tfoot>'.$labels.'</tfoot>';
+        }
+        $content .= '<tbody>';
+        foreach ( $values as $id => $vals ) {
+            $content .= '<tr>';
+            foreach ( $vals as $val ) {
+                $content .= '<td>'.$val.'</td>';
+            }
+            $content .= '</tr>';
+        }
+        $content .= '</tbody>';
+        $content .= '</table>';
+        $content .= $pagination;
+
+        return $content;
+    }
+
 }
 
 $wpuBasePlugin = new wpuBasePlugin();

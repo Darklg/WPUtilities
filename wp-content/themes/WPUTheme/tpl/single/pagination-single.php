49f387ee3477354cac14736a0566c317c1166753
Darklg <kev.rocher@gmail.com>
2013-11-20 08:07:45 +0100

    Single Pagination uses WordPress 3.7.0

--- wp-content/themes/WPUTheme/tpl/single/pagination-single.php
+++ wp-content/themes/WPUTheme/tpl/single/pagination-single.php
@@ -1,3 +1,16 @@
-<nav class="main-pagination">
-    <p><?php previous_post_link(); ?> | <?php next_post_link(); ?></p>
-</nav>
+<?php
+$previous_post_link = trim( get_previous_post_link( '&laquo; %link' ) );
+$next_post_link = trim( get_next_post_link( '%link &raquo;' ) );
+
+$hasALink = ( !empty( $previous_post_link ) || !empty( $next_post_link ) );
+$hasTwoLinks = ( !empty( $previous_post_link ) && !empty( $next_post_link ) );
+
+if ( $hasALink ) {
+    echo '<nav class="main-pagination"><p>';
+    echo $previous_post_link;
+    if ( $hasTwoLinks ) {
+        echo ' | ';
+    }
+    echo $next_post_link;
+    echo '</p></nav>';
+}

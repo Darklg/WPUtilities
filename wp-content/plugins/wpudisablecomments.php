e62753a31865fe30dccce3cf20b160a9ac5c1f69
Darklg <kev.rocher@gmail.com>
2013-12-01 15:47:00 +0100

    Disable comments v 1.0 : Remove comments from all post types / Disable Pings / Disable comments RSS feed

--- wp-content/plugins/wpudisablecomments.php
+++ wp-content/plugins/wpudisablecomments.php
@@ -3,7 +3,7 @@
 Plugin Name: WPU disable comments
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: Disable all comments
-Version: 0.3
+Version: 1.0
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -45,12 +45,16 @@ function wputh_disable_comments_admin_menus() {
 add_action( 'admin_menu', 'wputh_disable_comments_admin_menus' );
 
 /* ----------------------------------------------------------
-  Removes from post and pages
+  Removes from all post types
 ---------------------------------------------------------- */
 
 function wputh_disable_comments_support() {
-    remove_post_type_support( 'post', 'comments' );
-    remove_post_type_support( 'page', 'comments' );
+    $post_types = get_post_types(  array(
+            'public'   => true
+        ), 'names' );
+    foreach ( $post_types as $post_type ) {
+        remove_post_type_support( $post_type, 'comments' );
+    }
 }
 
 add_action( 'init', 'wputh_disable_comments_support', 100 );
@@ -75,3 +79,25 @@ function wputh_disable_comments_send_spam( $approved , $commentdata ) {
 }
 
 add_filter( 'pre_comment_approved' , 'wputh_disable_comments_send_spam' , '99', 2 );
+
+/* ----------------------------------------------------------
+  Disable pings
+---------------------------------------------------------- */
+
+function wputh_disable_comments_disable_ping( &$links ) {
+    $links = array();
+}
+
+add_action( 'pre_ping', 'wputh_disable_comments_disable_ping' );
+
+/* ----------------------------------------------------------
+  Disable comments RSS feed
+---------------------------------------------------------- */
+
+function wputh_disable_comments_feed_links() {
+    if ( function_exists( 'remove_theme_support' ) ) {
+        remove_theme_support( 'automatic-feed-links' );
+    }
+}
+
+add_action( 'init', 'wputh_disable_comments_feed_links' );

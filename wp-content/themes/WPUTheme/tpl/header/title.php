f3630df024aa70e3b60513e80e493d02a73e3094
Darklg <kev.rocher@gmail.com>
2013-12-26 17:45:40 +0100

    Theme - Simplified header templates

--- wp-content/themes/WPUTheme/tpl/header/title.php
+++
@@ -1,9 +0,0 @@
-<?php
-include dirname( __FILE__ ) . '/../../z-protect.php';
-
-$main_tag = 'div';
-if(is_home()){
-    $main_tag = 'h1';
-}
-
-?><<?php echo $main_tag; ?> class="h1 main-title"><a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a></<?php echo $main_tag; ?>>

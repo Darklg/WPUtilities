12d9312b67e6f65539cf39a524a7a475855e8143
Darklg <kev.rocher@gmail.com>
2013-12-26 17:38:58 +0100

    Updating CSSCommon

--- wp-content/themes/WPUTheme/tpl/header/searchform.php
+++ wp-content/themes/WPUTheme/tpl/header/searchform.php
@@ -2,7 +2,7 @@
 include dirname( __FILE__ ) . '/../../z-protect.php';
 ?><form role="search" method="get" id="header-search" class="header-search" action="<?php echo site_url(); ?>">
     <div>
-        <label class="suppression-element" for="header-search__input"><?php echo __( 'Search for:', 'wputh' ); ?></label>
+        <label class="cssc-remove-element" for="header-search__input"><?php echo __( 'Search for:', 'wputh' ); ?></label>
         <input type="text" value="" name="s" id="header-search__input" />
         <button class="cssc-button" id="header-search__submit"><?php echo __('Search', 'wputh'); ?></button>
     </div>

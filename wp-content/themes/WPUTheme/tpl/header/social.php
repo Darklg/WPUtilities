0ec54b54f787071ca2f6b8caea227724a1d2c414
Axel <axel@aquelito.fr>
2013-12-26 23:57:11 +0100

    Update HTML social links

--- wp-content/themes/WPUTheme/tpl/header/social.php
+++ wp-content/themes/WPUTheme/tpl/header/social.php
@@ -1,14 +1,22 @@
 <?php
+
 $social_links = array(
-    'twitter' => 'Twitter',
-    'facebook' => 'Facebook',
+    'twitter'   => 'Twitter',
+    'facebook'  => 'Facebook',
     'instagram' => 'Instagram',
 );
-echo '<ul class="header__social">';
-foreach ( $social_links as $id => $name ) {
-    $social_link = trim( get_option( 'social_'.$id.'_url' ) );
-    if ( !empty( $social_link ) ) {
-        echo '<li><a href="'.$social_link.'" class="'.$id.'" target="_blank">'.$name.'</a></li>';
-    }
+
+if(isset($social_links)) {
+  foreach ( $social_links as $id => $name ) {
+      $social_link = trim( get_option( 'social_'.$id.'_url' ) );
+      if ( !empty( $social_link ) ) {
+          $social_output[] = '<a href="'.$social_link.'" rel="me" class="'.$id.'" target="_blank" title="'. sprintf( __("%s : Follow %s (open in new window)", 'wputh'), $name, get_bloginfo('name')) .'">'.$name.'</a>';
+      }
+  }
+
+  if(isset($social_output) && is_array($social_output)) {
+      echo '<ul class="header__social">';
+      echo '<li>'. implode('</li><li>', $social_output) . '</li>';
+      echo '</ul>';
+  }
 }
-echo '</ul>';

96f3b42b2d3cff88187e1959878c127dfa09a52a
Axel <axel@aquelito.fr>
2013-12-26 22:40:53 +0100

    Add role-contentinfo - AW12.10.4

--- wp-content/themes/WPUTheme/footer.php
+++ wp-content/themes/WPUTheme/footer.php
@@ -3,11 +3,11 @@ include dirname( __FILE__ ) . '/z-protect.php';
 if (isset($_POST['is_ajax'])) return;
 ?>
 </div></div>
-<footer class="main-footer centered-container">
-    <div>
+<div class="main-footer centered-container">
+    <footer class="contentinfo" role="contentinfo" id="contentinfo">
         <?php include get_template_directory() . '/tpl/footer/copyright.php'; ?>
-    </div>
-</footer>
+    </footer>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>

c11055fd3047f5d0c59578b7fae4687c326c2397
Darklg <kev.rocher@gmail.com>
2013-12-28 18:33:18 +0100

    Theme - fix search form syntax // Thanks to @aquelito

--- wp-content/themes/WPUTheme/tpl/header/searchform.php
+++ wp-content/themes/WPUTheme/tpl/header/searchform.php
@@ -1,9 +1,12 @@
 <?php
 include dirname( __FILE__ ) . '/../../z-protect.php';
-?><form role="search" method="get" id="header-search" class="header-search" action="<?php echo site_url(); ?>">
-    <div>
-        <label class="cssc-remove-element" for="header-search__input"><?php echo __( 'Search for:', 'wputh' ); ?></label>
-        <input type="text" value="" name="s" id="header-search__input" />
-        <button class="cssc-button" id="header-search__submit"><?php echo __('Search', 'wputh'); ?></button>
-    </div>
-</form>
+?>
+<div class="search search--header header-search">
+  <form role="search" method="get" id="header-search" class="search__form" action="<?php echo site_url(); ?>">
+      <div class="search__inner">
+          <label class="cssc-remove-element" for="s"><?php _e( 'Search for:', 'wputh' ); ?></label>
+          <input type="text" value="" name="s" id="s" class="search__input" id="header-search__input" placeholder="<?php echo esc_attr__( 'Enter your keywords...', 'wputh' ); ?>" title="<?php echo esc_attr__( 'Search by keywords', 'wputh' ); ?>" />
+          <button class="search__submit cssc-button" id="search_submit" title="<?php echo sprintf( __( 'Search on %s', 'wputh' ), get_bloginfo('name') ); ?>"><?php _e('Search', 'wputh'); ?></button>
+      </div>
+  </form>
+</div>

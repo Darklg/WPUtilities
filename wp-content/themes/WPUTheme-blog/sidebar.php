d708e052944b4950cd82a0b27179a0d769430247
Darklg <kev.rocher@gmail.com>
2013-12-28 02:17:22 +0100

    Blog Theme - Fix sidebar protection

--- wp-content/themes/WPUTheme-blog/sidebar.php
+++ wp-content/themes/WPUTheme-blog/sidebar.php
@@ -1,5 +1,5 @@
 <?php
-include dirname( __FILE__ ) . '/z-protect.php';
+include dirname( __FILE__ ) . '/../WPUTheme/z-protect.php';
 if(!isset($_POST['is_ajax'])){
 ?><aside class="main-sidebar">
     <ul class="main-sidebar--widgets"><?php dynamic_sidebar( 'wputh-sidebar' );?></ul>

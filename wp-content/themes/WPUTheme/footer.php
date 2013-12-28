3e9bd16890a5e8763c6ac14bb73dffadfd9e92ff
Darklg <kev.rocher@gmail.com>
2013-12-28 02:17:05 +0100

    Theme - Merge Copyright in footer

--- wp-content/themes/WPUTheme/footer.php
+++ wp-content/themes/WPUTheme/footer.php
@@ -1,11 +1,16 @@
 <?php
 include dirname( __FILE__ ) . '/z-protect.php';
-if (isset($_POST['is_ajax'])) return;
+if ( isset( $_POST['is_ajax'] ) ) return;
 ?>
 </div></div>
 <footer class="main-footer centered-container">
     <div>
-        <?php include get_template_directory() . '/tpl/footer/copyright.php'; ?>
+        <small class="main-footer--copyright">&copy; <?php echo date( 'Y' ); ?> - <?php bloginfo( 'name' ); ?></small>
+        <small class="main-footer--credits"><?php
+        echo sprintf( __( 'A %s site using %s', 'wputh' ),
+            '<a href="https://wordpress.org" target="_blank">WordPress</a>',
+            '<a href="https://github.com/Darklg/WPUtilities" target="_blank">WPUtilities</a>' );
+        ?></small>
     </div>
 </footer>
 <?php wp_footer(); ?>
--- wp-content/themes/WPUTheme/tpl/footer/copyright.php
+++
@@ -1,2 +0,0 @@
-<small class="main-footer--copyright">&copy; <?php echo date('Y'); ?> - <?php bloginfo('name'); ?></small>
-<small class="main-footer--credits"><?php echo sprintf(__( 'A %s site using %s', 'wputh' ), '<a href="https://wordpress.org" target="_blank">WordPress</a>', '<a href="https://github.com/Darklg/WPUtilities" target="_blank">WPUtilities</a>'); ?> </small>

cd87dc623fc3ded2514ec17c0c483b501ed129d8
Darklg <kev.rocher@gmail.com>
2013-12-28 18:22:07 +0100

    Theme - Social links with title & rel // Thanks to @aquelito

--- wp-content/themes/WPUTheme/tpl/header/social.php
+++ wp-content/themes/WPUTheme/tpl/header/social.php
@@ -8,7 +8,7 @@ echo '<ul class="header__social">';
 foreach ( $social_links as $id => $name ) {
     $social_link = trim( get_option( 'social_'.$id.'_url' ) );
     if ( !empty( $social_link ) ) {
-        echo '<li><a href="'.$social_link.'" class="'.$id.'" target="_blank">'.$name.'</a></li>';
+        echo '<li><a rel="me" href="'.$social_link.'" class="'.$id.'" title="'.sprintf( __( '%s: Follow %s (open in new window)', 'wputh' ), $name, get_bloginfo( 'name' ) ).'" target="_blank">'.$name.'</a></li>';
     }
 }
 echo '</ul>';

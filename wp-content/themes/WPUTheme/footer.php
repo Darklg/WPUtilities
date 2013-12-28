dd87430aa230d0d9814e2b6d940582b8fe18088f
Axel <axel@aquelito.fr>
2013-12-28 18:14:09 +0100

    [PATCH] Add role-contentinfo - AW12.10.4

--- wp-content/themes/WPUTheme/footer.php
+++ wp-content/themes/WPUTheme/footer.php
@@ -3,16 +3,16 @@ include dirname( __FILE__ ) . '/z-protect.php';
 if ( isset( $_POST['is_ajax'] ) ) return;
 ?>
 </div></div>
-<footer class="main-footer centered-container">
-    <div>
+<div class="main-footer centered-container">
+    <footer class="contentinfo" role="contentinfo" id="contentinfo">
         <small class="main-footer--copyright">&copy; <?php echo date( 'Y' ); ?> - <?php bloginfo( 'name' ); ?></small>
         <small class="main-footer--credits"><?php
         echo sprintf( __( 'A %s site using %s', 'wputh' ),
             '<a href="https://wordpress.org" target="_blank">WordPress</a>',
             '<a href="https://github.com/Darklg/WPUtilities" target="_blank">WPUtilities</a>' );
         ?></small>
-    </div>
-</footer>
+    </footer>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>

030f87cb45365a86b874270a620cf93c6d611440
Darklg <kev.rocher@gmail.com>
2014-01-10 00:32:59 +0100

    Theme - Utilities : Sendmail - Attachments, variables, models

--- wp-content/themes/WPUTheme/inc/theme/utilities.php
+++ wp-content/themes/WPUTheme/inc/theme/utilities.php
@@ -174,7 +174,23 @@ function wputh_get_attachments_images( $postID, $format='medium' ) {
  * @param string  $subject
  * @param string  $content
  */
-function wputh_sendmail( $address, $subject, $content ) {
+function wputh_sendmail( $address, $subject, $content, $more = array() ) {
+
+    // Set "more" default values values
+    if ( !is_array( $more ) ) {
+        $more = array();
+    }
+    $ids = array( 'headers', 'attachments', 'vars' );
+    foreach ( $ids as $id ) {
+        if ( !isset( $more[$id] ) || !is_array( $more[$id] ) ) {
+            $more[$id] = array();
+        }
+    }
+    if ( !isset( $more['model'] ) ) {
+        $more['model'] = '';
+    }
+
+    // Include headers
     $tpl_mail = get_template_directory() . '/tpl/mails/';
     $mail_content = '';
     if ( file_exists( $tpl_mail.'header.php' ) ) {
@@ -183,7 +199,15 @@ function wputh_sendmail( $address, $subject, $content ) {
         $mail_content .= ob_get_clean();
     }
 
-    $mail_content .= $content;
+    $model = $tpl_mail.'model-'.$more['model'].'.php';
+    if ( !empty( $more['model'] ) && file_exists( $model ) ) {
+        ob_start();
+        include $model;
+        $mail_content .= ob_get_clean();
+    }
+    else {
+        $mail_content .= $content;
+    }
 
     if ( file_exists( $tpl_mail.'footer.php' ) ) {
         ob_start();
@@ -192,7 +216,7 @@ function wputh_sendmail( $address, $subject, $content ) {
     }
 
     add_filter( 'wp_mail_content_type', 'wputh_sendmail_set_html_content_type' );
-    wp_mail( $address, '[' . get_bloginfo( 'name' ) . '] ' . $subject, $mail_content );
+    wp_mail( $address, '[' . get_bloginfo( 'name' ) . '] ' . $subject, $mail_content, $more['headers'], $more['attachments'] );
     // reset content-type to to avoid conflicts -- http://core.trac.wordpress.org/ticket/23578
     remove_filter( 'wp_mail_content_type', 'wputh_sendmail_set_html_content_type' );
 }

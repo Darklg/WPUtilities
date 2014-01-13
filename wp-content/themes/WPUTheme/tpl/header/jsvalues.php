1e76144fac7a270decbb7e1b97de0e2099989282
Darklg <kev.rocher@gmail.com>
2014-01-13 23:14:40 +0100

    Theme - Cleaner JS Values

--- wp-content/themes/WPUTheme/tpl/header/jsvalues.php
+++ wp-content/themes/WPUTheme/tpl/header/jsvalues.php
@@ -1,5 +1,2 @@
 <script>var wputh_pagination_kind = '<?php echo PAGINATION_KIND; ?>';</script>
-<div id="js-values"
-data-page_title="<?php echo addslashes( trim( get_bloginfo( 'name' ) . wp_title( ' - ', false ) ) ); ?>"
-data-body_<?php body_class( 'cssc-is-responsive' ); ?>
-></div>
+<div id="js-values" class="js-values" data-page_title="<?php echo addslashes( trim( wp_title( ) ) ); ?>" data-body_<?php body_class( 'cssc-is-responsive' ); ?>></div>

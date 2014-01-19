3e45045febb8249936fa8aeb4241f08bfa64cd46
Darklg <kev.rocher@gmail.com>
2014-01-19 23:38:25 +0100

    Theme - allow full options for scripts loading & separation between header & footer for default scripts

--- wp-content/themes/WPUTheme/inc/assets/scripts.php
+++ wp-content/themes/WPUTheme/inc/assets/scripts.php
@@ -10,24 +10,39 @@ function wputh_add_javascripts() {
             'uri' => '/js/lib/mootools-more-1.4.0.1.js'
         ),
         'dk-smooth-scroll' => array(
-            'uri' => '/js/classes/dk-smooth-scroll.js'
+            'uri' => '/js/classes/dk-smooth-scroll.js',
+            'footer' => 1
         ),
         'functions' => array(
-            'uri' => '/js/functions.js'
+            'uri' => '/js/functions.js',
+            'footer' => 1
         ),
         'wpu-home' => array(
-            'uri' => '/js/modules/home.js'
+            'uri' => '/js/modules/home.js',
+            'footer' => 1
         ),
         'wpu-faq' => array(
-            'uri' => '/js/modules/faq.js'
+            'uri' => '/js/modules/faq.js',
+            'footer' => 1
         ),
         'events' => array(
-            'uri' => '/js/events.js'
+            'uri' => '/js/events.js',
+            'footer' => 1
         )
     );
 
     foreach ( $JScripts as $id => $details ) {
-        wp_register_script( $id, get_template_directory_uri() . $details['uri'] );
+        $url = '';
+        if ( isset( $details['url'] ) ) {
+            $url = $details['url'];
+        }
+        if ( isset( $details['uri'] ) ) {
+            $url = get_template_directory_uri() . $details['uri'];
+        }
+        $deps = isset( $details['deps'] ) ? $details['deps'] : false;
+        $ver = isset( $details['ver'] ) ? $details['ver'] : false;
+        $in_footer = isset( $details['footer'] ) && $details['footer'] == true;
+        wp_register_script( $id, $url, $deps, $ver, $in_footer );
         wp_enqueue_script( $id );
     }
 }

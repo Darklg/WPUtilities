8564c0996fb1292789035090749b14bdc9338c3d
Darklg <kev.rocher@gmail.com>
2014-01-22 18:11:39 +0100

    WP Utilities Open Sans v 0.1.1 : Avoid conflict in function name

--- wp-content/plugins/wpu_opensans/wpu_opensans.php
+++ wp-content/plugins/wpu_opensans/wpu_opensans.php
@@ -2,15 +2,15 @@
 /*
 Plugin Name: WP Utilities Open Sans
 Description: Use an embedded version of the font Open Sans, and bypasses Google Fonts in WP Admin
-Version: 0.1
+Version: 0.1.1
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 */
 
-add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
-function load_custom_wp_admin_style() {
+add_action( 'admin_enqueue_scripts', 'wpu_opensans_enqueue_style' );
+function wpu_opensans_enqueue_style() {
     wp_deregister_style( 'open-sans' );
     wp_register_style( 'open-sans', plugins_url( '/assets/css/open-sans.css', __FILE__ ) );
 }

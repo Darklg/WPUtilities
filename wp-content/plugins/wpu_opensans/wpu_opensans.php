a91ead799278e9be4ff912d49815fd5d1dde4fba
Darklg <kev.rocher@gmail.com>
2014-01-26 23:20:29 +0100

    Open Sans v 0.1.3 : Fix Google fonts call in front
    Thanks @ScreenFeedFr !

--- wp-content/plugins/wpu_opensans/wpu_opensans.php
+++ wp-content/plugins/wpu_opensans/wpu_opensans.php
@@ -2,13 +2,14 @@
 /*
 Plugin Name: WP Utilities Open Sans
 Description: Use an embedded version of the font Open Sans, and bypasses Google Fonts in WP Admin
-Version: 0.1.2
+Version: 0.1.3
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 */
 
+add_action( 'wp_enqueue_scripts', 'wpu_opensans_enqueue_style' );
 add_action( 'admin_enqueue_scripts', 'wpu_opensans_enqueue_style' );
 function wpu_opensans_enqueue_style() {
     wp_deregister_style( 'open-sans' );

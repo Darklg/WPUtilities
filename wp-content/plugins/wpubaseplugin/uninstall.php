18f0dc8d0ecce63f8628f4d4c98c9352cb9cf0df
Darklg <kev.rocher@gmail.com>
2014-03-16 19:52:28 +0100

    Removing Base Plugin

--- wp-content/plugins/wpubaseplugin/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-//if uninstall not called from WordPress exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-    exit();
-
-include dirname( __FILE__ ).'/wpubaseplugin.php';
-
-$wpuBasePlugin->uninstall();

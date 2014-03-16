ac08a075eb7e26e310f27c26246a208a08f8e0ec
Darklg <kev.rocher@gmail.com>
2014-03-16 20:06:32 +0100

    Removing WPU Maintenance, WPU Track 404 & Search, WP Utilities Newsletter

--- wp-content/plugins/wpunewsletter/assets/script.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-    // Special checkbox
-    jQuery(".wpunewsletter_element_check").on("change", function(e) {
-        jQuery('.wpunewsletter_element, .wpunewsletter_element_check').attr('checked', jQuery(this).is(':checked'));
-    });
-});

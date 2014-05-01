a0cb4c580b05c61886112994454cd7cb21fc7273
Darklg <kev.rocher@gmail.com>
2014-05-01 23:07:20 +0200

    fix JS search form check

--- wp-content/themes/WPUTheme/js/functions.js
+++ wp-content/themes/WPUTheme/js/functions.js
@@ -7,7 +7,7 @@ var search_form_check = function() {
     var el_search_form = $('header-search');
     if (el_search_form) {
         el_search_form.addEvent('submit', function(e) {
-            var input = $('header-search__input');
+            var input = $('s');
             if (!input || input.get('value').trim() === '') {
                 e.preventDefault();
             }

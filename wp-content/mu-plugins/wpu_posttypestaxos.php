495ea0d7aab96e2c91ef6a148f11b66d33ec65c9
Darklg <kev.rocher@gmail.com>
2014-12-02 16:06:43 +0100

    WPU Post Types & Taxonomies v 0.5 : Add parameter to hide a post type in front (but allow query)

--- wp-content/mu-plugins/wpu_posttypestaxos.php
+++ wp-content/mu-plugins/wpu_posttypestaxos.php
@@ -1,8 +1,9 @@
 <?php
+
 /*
 Plugin Name: WPU Post types & taxonomies
 Description: Load custom post types & taxonomies
-Version: 0.4
+Version: 0.5
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -24,19 +25,30 @@ class wputh_add_post_types_taxonomies
         'has_archive',
         'public',
         'publicly_queryable',
+        'query_var',
         'rewrite',
         'show_ui',
         'with_front'
     );
 
     function __construct() {
-        add_action( 'init', array( &$this, 'add_post_types' ) );
-        add_action( 'init', array( &$this, 'add_taxonomies' ) );
+        add_action('init', array(&$this,
+            'add_post_types'
+        ));
+        add_action('init', array(&$this,
+            'add_taxonomies'
+        ));
 
-        if ( is_admin() ) {
-            add_filter( 'manage_posts_columns', array( &$this, 'columns_head_taxo' ), 10 );
-            add_action( 'manage_posts_custom_column', array( &$this, 'columns_content_taxo' ), 10, 2 );
-            add_action( 'pre_get_posts', array( &$this, 'add_editor_styles' ));
+        if (is_admin()) {
+            add_filter('manage_posts_columns', array(&$this,
+                'columns_head_taxo'
+            ) , 10);
+            add_action('manage_posts_custom_column', array(&$this,
+                'columns_content_taxo'
+            ) , 10, 2);
+            add_action('pre_get_posts', array(&$this,
+                'add_editor_styles'
+            ));
         }
     }
 
@@ -65,6 +77,15 @@ class wputh_add_post_types_taxonomies
                     'thumbnail'
                 )
             );
+
+            // Hide only in front
+            if (isset($post_type['wputh__hide_front']) && is_bool($post_type['wputh__hide_front']) && $post_type['wputh__hide_front'] !== false) {
+                $args['has_archive'] = false;
+                $args['public'] = false;
+                $args['publicly_queryable'] = false;
+                $args['query_var'] = false;
+                $args['rewrite'] = false;
+            }
 
             // Default label: slug
             if (!isset($post_type['name'])) {
@@ -144,18 +165,16 @@ class wputh_add_post_types_taxonomies
     ---------------------------------------------------------- */
 
     public function add_taxonomies() {
-        $taxonomies = apply_filters( 'wputh_get_taxonomies', array() );
-        $taxonomies = $this->verify_taxonomies( $taxonomies );
-        foreach ( $taxonomies as $slug => $taxo ) {
-            register_taxonomy(
-                $slug,
-                $taxo['post_type'],
-                array(
-                    'label' => $taxo['name'],
-                    'rewrite' => array( 'slug' => $slug ),
-                    'hierarchical' => $taxo['hierarchical']
-                )
-            );
+        $taxonomies = apply_filters('wputh_get_taxonomies', array());
+        $taxonomies = $this->verify_taxonomies($taxonomies);
+        foreach ($taxonomies as $slug => $taxo) {
+            register_taxonomy($slug, $taxo['post_type'], array(
+                'label' => $taxo['name'],
+                'rewrite' => array(
+                    'slug' => $slug
+                ) ,
+                'hierarchical' => $taxo['hierarchical']
+            ));
         }
     }
 
@@ -163,15 +182,19 @@ class wputh_add_post_types_taxonomies
       Verify taxonomies
     ---------------------------------------------------------- */
 
-    private function verify_taxonomies( $taxonomies ) {
-        foreach ( $taxonomies as $slug => $taxo ) {
-            $post_type = ( isset( $taxo['post_type'] ) ? $taxo['post_type'] : array( 'post' ) );
-            if ( !is_array( $post_type ) ) {
-                $post_type = array( $post_type );
+    private function verify_taxonomies($taxonomies) {
+        foreach ($taxonomies as $slug => $taxo) {
+            $post_type = (isset($taxo['post_type']) ? $taxo['post_type'] : array(
+                'post'
+            ));
+            if (!is_array($post_type)) {
+                $post_type = array(
+                    $post_type
+                );
             }
             $taxonomies[$slug]['post_type'] = $post_type;
-            $taxonomies[$slug]['hierarchical'] = isset( $taxo['hierarchical'] ) ? $taxo['hierarchical'] : true;
-            $taxonomies[$slug]['admin_column'] = isset( $taxo['admin_column'] ) ? $taxo['admin_column'] : true;
+            $taxonomies[$slug]['hierarchical'] = isset($taxo['hierarchical']) ? $taxo['hierarchical'] : true;
+            $taxonomies[$slug]['admin_column'] = isset($taxo['admin_column']) ? $taxo['admin_column'] : true;
         }
         return $taxonomies;
     }
@@ -180,19 +203,21 @@ class wputh_add_post_types_taxonomies
       Add taxonomy columns
     ---------------------------------------------------------- */
 
-    public function columns_head_taxo( $defaults ) {
+    public function columns_head_taxo($defaults) {
         global $post;
+
         // Isolate latest value
-        $last_key = key( array_slice( $defaults, -1, 1, TRUE ) );
-        $last_value = $defaults[ $last_key ];
-        unset( $defaults[ $last_key ] );
+        $last_key = key(array_slice($defaults, -1, 1, TRUE));
+        $last_value = $defaults[$last_key];
+        unset($defaults[$last_key]);
 
-        $taxonomies = apply_filters( 'wputh_get_taxonomies', array() );
-        $taxonomies = $this->verify_taxonomies( $taxonomies );
+        $taxonomies = apply_filters('wputh_get_taxonomies', array());
+        $taxonomies = $this->verify_taxonomies($taxonomies);
 
-        foreach ( $taxonomies as $slug => $taxo ) {
+        foreach ($taxonomies as $slug => $taxo) {
+
             // Add keys
-            if ( $taxo['admin_column'] && isset( $post->post_type ) && in_array( $post->post_type, $taxo['post_type'] ) ) {
+            if ($taxo['admin_column'] && isset($post->post_type) && in_array($post->post_type, $taxo['post_type'])) {
                 $defaults[$slug] = $taxo['name'];
             }
         }
@@ -202,27 +227,29 @@ class wputh_add_post_types_taxonomies
         return $defaults;
     }
 
-    public function columns_content_taxo( $column_name, $post_id ) {
+    public function columns_content_taxo($column_name, $post_id) {
         global $post;
-        if ( !isset( $post->post_type ) ) {
+        if (!isset($post->post_type)) {
             return;
         }
-        $taxonomies = apply_filters( 'wputh_get_taxonomies', array() );
-        $taxonomies = $this->verify_taxonomies( $taxonomies );
+        $taxonomies = apply_filters('wputh_get_taxonomies', array());
+        $taxonomies = $this->verify_taxonomies($taxonomies);
 
-        foreach ( $taxonomies as $slug => $taxo ) {
-            if ( $column_name == $slug && in_array( $post->post_type, $taxo['post_type'] ) ) {
-                $terms = wp_get_post_terms( $post_id, $slug );
+        foreach ($taxonomies as $slug => $taxo) {
+            if ($column_name == $slug && in_array($post->post_type, $taxo['post_type'])) {
+                $terms = wp_get_post_terms($post_id, $slug);
                 $content_term = array();
-                if ( is_array( $terms ) ) {
-                    foreach ( $terms as $term ) {
-                        $content_term[] = '<a href="'.admin_url( 'edit.php?post_type='.$post->post_type.'&'.$slug.'='.$term->slug ) . '">'.$term->name.'</a>';
+                if (is_array($terms)) {
+                    foreach ($terms as $term) {
+                        $content_term[] = '<a href="' . admin_url('edit.php?post_type=' . $post->post_type . '&' . $slug . '=' . $term->slug) . '">' . $term->name . '</a>';
                     }
                 }
-                if ( empty( $content_term ) ) {
-                    $content_term = array( '-' );
+                if (empty($content_term)) {
+                    $content_term = array(
+                        '-'
+                    );
                 }
-                echo implode( ', ', $content_term );
+                echo implode(', ', $content_term);
             }
         }
     }

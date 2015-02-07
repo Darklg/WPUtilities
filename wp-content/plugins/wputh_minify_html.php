570977c1a2d96f0e510f127565c0f79987943a45
Darklg <kev.rocher@gmail.com>
2015-02-07 22:55:25 +0100

    Minify HTML v 0.2 — Add correct code structure + Isolate some tags

--- wp-content/plugins/wputh_minify_html.php
+++ wp-content/plugins/wputh_minify_html.php
@@ -1,26 +1,72 @@
 <?php
+
 /*
 Plugin Name: WP Utilities Minify HTML
 Description: Minify HTML
-Version: 0.1.1
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 */
 
-if ( !WP_DEBUG ) {
-    add_action( 'init', 'wputh_minify_html' );
+class WPUMinifyHTML
+{
+    private $filter_tags = array(
+        'code',
+        'kbd',
+        'pre',
+        'samp',
+        'script',
+        'style',
+    );
+    function __construct() {
+        add_action('template_redirect', array(&$this,
+            'set_ob_action'
+        ));
+    }
+
+    function set_ob_action() {
+        ob_start(array(&$this,
+            'minify_html'
+        ));
+    }
+
+    function minify_html($html) {
+
+        $noncompressedtags = array();
+        $noncompressedtagsi = 0;
+
+        // Isolate some tags
+        foreach ($this->filter_tags as $tag) {
+            preg_match_all('/<' . $tag . '(.*)<\/' . $tag . '>/isU', $html, $matches);
+            foreach ($matches[0] as $match) {
+                $tag_match = array(
+                    $match,
+                    '<______tag_' . $noncompressedtagsi . '____/>'
+                );
+                $noncompressedtags[] = $tag_match;
+                $noncompressedtagsi++;
+                $html = str_replace($match, $tag_match[1], $html);
+            }
+        }
+
+        // Removing multiple spaces
+        $html = preg_replace('/(\s{2,})/', ' ', $html);
+
+        // Removing spaces between tags
+        $html = preg_replace('/>(\s+)</', '><', $html);
+
+        // Replace isolated tags
+        foreach ($noncompressedtags as $item) {
+            $html = str_replace($item[1], $item[0], $html);
+        }
+
+        return $html;
+    }
 }
 
-function wputh_minify_html() {
-    ob_start( 'wputh_return_minify_html' );
+if (!WP_DEBUG) {
+    $WPUMinifyHTML = new WPUMinifyHTML();
 }
 
-function wputh_return_minify_html( $html ) {
-    // Removing multiple spaces
-    $html = preg_replace( '/(\s{2,})/', ' ', $html );
-    // Removing spaces between tags
-    $html = preg_replace( '/>(\s+)</', '><', $html );
-    return $html;
-}

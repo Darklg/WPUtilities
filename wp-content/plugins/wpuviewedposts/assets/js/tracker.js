5889d83b64a59fe0347ea10091cbd577d518455b
Darklg <kev.rocher@gmail.com>
2015-09-10 18:57:10 +0200

    Remove WPU Viewed Posts

--- wp-content/plugins/wpuviewedposts/assets/js/tracker.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    var data = {
-        'action': 'wpuviewedposts_track_view',
-        'date': Date.now() ,
-        'post_id': ajax_object.post_id
-    };
-    jQuery.post(ajax_object.ajax_url, data, function(response) {});
-});

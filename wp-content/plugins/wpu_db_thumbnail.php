566befa94f467491e7134b3711fda0d6a74c3ffc
Kevin Rocher <kevin@rocher-dev.com>
2016-09-21 23:10:43 +0200

    DB Thumbnail v 0.1 - Store a small thumbnail in db

---
+++ wp-content/plugins/wpu_db_thumbnail.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+Plugin Name: WPU DB Thumbnail
+Description: Store a small thumbnail in db
+Version: 0.1
+Author: Darklg
+Author URI: http://darklg.me/
+License: MIT License
+License URI: http://opensource.org/licenses/MIT
+*/
+
+add_action('save_post', 'wpudbthumbnail_init');
+function wpudbthumbnail_init($post_id = false) {
+    if (!is_numeric($post_id) || wp_is_post_revision($post_id)) {
+        return false;
+    }
+
+    $post_thumbnail_id = get_post_thumbnail_id($post_id);
+    if (!is_numeric($post_thumbnail_id)) {
+        return false;
+    }
+
+    $base_image = get_attached_file($post_thumbnail_id);
+    $image = wp_get_image_editor($base_image);
+    if (is_wp_error($image)) {
+        return;
+    }
+
+    /* Generate image */
+    $type = pathinfo($base_image, PATHINFO_EXTENSION);
+    $upload_dir = wp_upload_dir();
+    $tmp_file = $upload_dir['basedir'] . '/tmp-thumb-' . microtime() . '.png';
+    $image->resize(3);
+    $image->save($tmp_file, 'image/png');
+    $data = file_get_contents($tmp_file);
+    unlink($tmp_file);
+
+    /* Save as base64 */
+    $base64 = 'data:image/png;base64,' . base64_encode($data);
+    update_post_meta($post_id, 'wpudbthumbnail_base64thumb', $base64);
+}

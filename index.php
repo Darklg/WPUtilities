01838fc7ded868620c2bc51675bfc68d5dd03420
Kevin Rocher <kevin@rocher-dev.com>
2016-10-08 23:58:42 +0200

    Do not ignore main php file

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+require dirname(__FILE__) . '/wp-cms/wp-blog-header.php';

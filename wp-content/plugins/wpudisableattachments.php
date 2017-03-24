fd09b2982a04a5e8febfb80ee3326938e583670b
Kevin <kevin@rocher-dev.com>
2017-03-24 19:51:20 +0100

    Disable Attachments v 0.2.0
    * Fix an error with medias without parents.
    * Hide display link in admin.

--- wp-content/plugins/wpudisableattachments.php
+++ wp-content/plugins/wpudisableattachments.php
@@ -3,20 +3,32 @@
 Plugin Name: WPU disable attachments pages
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: Disable all attachments
-Version: 0.1
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 */
 
-add_action('template_redirect', 'wpudisableattachments_template_redirect');
-function wpudisableattachments_template_redirect() {
-    global $post;
-    if (!is_object($post) || $post->post_type != 'attachment' || !is_numeric($post->post_parent)) {
-        return;
+class WPUDisableAttachments {
+    public function __construct() {
+        add_action('template_redirect', array(&$this, 'template_redirect'));
+        add_action('admin_head', array(&$this, 'admin_head'));
     }
-    $post_parent_url = get_permalink($post->post_parent);
-    wp_redirect($post_parent_url);
-    die;
+
+    public function template_redirect() {
+        global $post;
+        if (!is_object($post) || $post->post_type != 'attachment' || !is_numeric($post->post_parent)) {
+            return;
+        }
+        $post_parent_url = $post->post_parent == 0 ? home_url() : get_permalink($post->post_parent);
+        wp_redirect($post_parent_url);
+        die;
+    }
+
+    public function admin_head() {
+        echo '<style>#post_type[value="attachment"] ~ #poststuff #edit-slug-box,.attachment-info .actions a.view-attachment {display:none;}</style>';
+    }
 }
+
+$WPUDisableAttachments = new WPUDisableAttachments();

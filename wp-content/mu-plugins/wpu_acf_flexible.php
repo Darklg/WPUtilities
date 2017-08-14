a4c4f57d244a0a14a5907ba9ace2a012e0879404
Kevin <kevin@rocher-dev.com>
2017-08-14 13:04:52 +0200

    v 0.2.0
    * Init default content files.
    * Fix layouts.

--- wp-content/mu-plugins/wpu_acf_flexible.php
+++ wp-content/mu-plugins/wpu_acf_flexible.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU ACF Flexible
 Description: Quickly generate flexible content in ACF
-Version: 0.1.0
+Version: 0.2.0
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -26,22 +26,41 @@ class wpu_acf_flexible {
             'class' => '',
             'id' => ''
         ),
-        'default_value' => '',
-        'placeholder' => '',
-        'prepend' => '',
+        'ajax' => 0,
+        'allow_null' => 0,
         'append' => '',
-        'maxlength' => '',
-        'min' => '',
-        'preview_size' => 'thumbnail',
+        'default_value' => '',
+        'display' => 'block',
         'library' => 'all',
         'max' => '',
-        'display' => 'block',
-        'allow_null' => 0,
+        'max_height' => '',
+        'max_size' => '',
+        'max_width' => '',
+        'maxlength' => '',
+        'mime_types' => '',
+        'min' => '',
+        'min_height' => '',
+        'min_size' => '',
+        'min_width' => '',
         'multiple' => 0,
-        'ui' => 0,
-        'ajax' => 0,
-        'sub_fields' => array()
+        'placeholder' => '',
+        'prepend' => '',
+        'preview_size' => 'thumbnail',
+        'sub_fields' => array(),
+        'ui' => 0
     );
+
+    private $default_content = <<<EOT
+<?php
+###varsblockid###
+?><div class="centered-container cc-block--testblockid">
+    <div class="block--testblockid">
+###valuesblockid###
+    </div>
+</div>
+
+
+EOT;
 
     public function __construct() {
         add_action('init', array(&$this, 'init'));
@@ -53,11 +72,11 @@ class wpu_acf_flexible {
         }
         $contents = apply_filters('wpu_acf_flexible_content', array());
         foreach ($contents as $id => $content) {
-            $this->add_field_group($id, $content['name'], $content['post_types'], $content['layouts']);
+            $this->add_field_group($id, $content);
         }
     }
 
-    public function set_field($id, $field) {
+    public function set_field($id, $field, $field_id) {
         $acf_field = $this->base_field;
 
         /* Label */
@@ -67,9 +86,7 @@ class wpu_acf_flexible {
         if (!isset($field['title'])) {
             $field['title'] = $field['label'];
         }
-        if (!isset($field['name'])) {
-            $field['name'] = $field['label'];
-        }
+        $field['name'] = $field_id;
 
         /* Return */
         if (isset($field['type'])) {
@@ -78,6 +95,9 @@ class wpu_acf_flexible {
             }
             if ($field['type'] == 'image' && !isset($field['return_format'])) {
                 $field['return_format'] = 'id';
+            }
+            if ($field['type'] == 'color') {
+                $field['type'] = 'color_picker';
             }
         }
 
@@ -89,7 +109,7 @@ class wpu_acf_flexible {
         if (isset($acf_field['sub_fields']) && is_array($acf_field['sub_fields'])) {
             $sub_fields = array();
             foreach ($acf_field['sub_fields'] as $sub_field_id => $sub_field) {
-                $sub_fields[$sub_field_id] = $this->set_field($id . $sub_field_id, $sub_field);
+                $sub_fields[$sub_field_id] = $this->set_field($id . $sub_field_id, $sub_field, $sub_field_id);
             }
             $acf_field['sub_fields'] = $sub_fields;
         }
@@ -97,13 +117,41 @@ class wpu_acf_flexible {
 
     }
 
-    public function add_field_group($content_id, $content_name, $post_types = array(), $layouts = array()) {
+    public function add_field_group($content_id, $content = array()) {
+        $content_name = (isset($content['name']) && !empty($content['name'])) ? $content['name'] : 'Default';
+        $post_types = (isset($content['post_types']) && is_array($content['post_types'])) ? $content['post_types'] : array('post');
+        $layouts = (isset($content['layouts']) && is_array($content['layouts'])) ? $content['layouts'] : array();
+
         /* Build Layouts */
         $layouts_acf = array();
         foreach ($layouts as $layout_id => $layout) {
             $layout_key = md5($content_id . $layout_id);
-            $layouts_acf[$layout_key] = $this->set_field($layout_key, $layout);
+            $layouts_acf[$layout_key] = $this->set_field($layout_key, $layout, $layout_id);
             unset($layouts_acf[$layout_key]['type']);
+        }
+
+        /* Init */
+        if (isset($content['init_files']) && $content['init_files']) {
+            foreach ($layouts as $layout_id => $layout) {
+
+                $vars = '';
+                $values = '';
+                foreach ($layout['sub_fields'] as $id => $sub_field) {
+                    if (!isset($sub_field['type']) || $sub_field['type'] == 'text') {
+                        $vars .= '$' . $id . ' = get_sub_field(\'' . $id . '\');' . "\n";
+                        $values .= '<div><?php echo $' . $id . ' ?></div>' . "\n";
+                    }
+                }
+
+                $content = str_replace('###varsblockid###', $vars, $this->default_content);
+                $content = str_replace('###valuesblockid###', $values, $content);
+                $content = str_replace('testblockid', $layout_id, $content);
+
+                $file_id = get_stylesheet_directory() . '/tpl/blocks/' . $layout_id . '.php';
+                if (!file_exists($file_id)) {
+                    file_put_contents($file_id, $content);
+                }
+            }
         }
 
         /* Build post types */

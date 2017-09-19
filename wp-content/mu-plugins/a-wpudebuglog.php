c4c4750602dc6ba42726434418a930987f4c8efb
Kevin <kevin@rocher-dev.com>
2017-09-19 23:55:36 +0200

    WPU Error Log v 0.1.0
    * Set a custom path for error log.

---
+++ wp-content/mu-plugins/a-wpudebuglog.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Plugin Name: WPU Error Log
+Description: Set a custom path for error log
+Version: 0.1.0
+Author: Darklg
+Author URI: http://darklg.me/
+License: MIT License
+License URI: http://opensource.org/licenses/MIT
+*/
+
+class WPUErrorLog {
+    public function __construct() {
+        if (!defined('WPUERRORLOGPATH')) {
+            $upload_dir = wp_upload_dir();
+            $log_path = apply_filters('beauxartsgli_log_path', $upload_dir['basedir']);
+        } else {
+            $log_path = WPUERRORLOGPATH;
+        }
+        if (!is_dir($log_path)) {
+            mkdir($log_path);
+        }
+        $log_file = $log_path . '/debug-' . date('Y-m-d') . '.log';
+        if (!file_exists($log_file)) {
+            touch($log_file);
+        }
+        ini_set('log_errors', 1);
+        ini_set('error_log', $log_file);
+    }
+}
+
+try {
+    $WPUErrorLog = new WPUErrorLog();
+} catch (Exception $e) {}

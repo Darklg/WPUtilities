eb64f0ebd55796bfe3dd13b0e6dc9e2a4560efd8
Kevin <kevin@rocher-dev.com>
2017-09-20 16:50:41 +0200

    WPU Woo Ask Invoice v 0.2.0
    * Add multiple statuses.
    * Enable multiple ask.
    * Fix order id.

--- wp-content/mu-plugins/wpu_woo_ask_invoice.php
+++ wp-content/mu-plugins/wpu_woo_ask_invoice.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU Woo Ask Invoice
 Description: Add an invoice by email
-Version: 0.1.0
+Version: 0.2.0
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -14,9 +14,16 @@ Based on : https://iconicwp.com/add-custom-page-account-area-woocommerce/
 class WPUWooAskInvoice {
 
     public $text_ask = '';
+    public $text_ask_multiple = '';
     public $text_success_ask = '';
     public $text_email_title = '';
     public $text_email_content = '';
+    public $switch_meta_key = 'wpuwooaskinvoice_asked';
+    public $enable_multiple_ask = true;
+    public $authorized_statuses = array(
+        'processing',
+        'on-hold'
+    );
 
     public function __construct() {
         add_action('plugins_loaded', array(&$this, 'plugins_loaded'));
@@ -25,40 +32,53 @@ class WPUWooAskInvoice {
     }
 
     public function plugins_loaded() {
+        /* Text */
         $this->text_ask = apply_filters('wpuwooaskinvoice__text_ask', __('Ask Invoice', 'wpuwooaskinvoice'));
+        $this->text_ask_multiple = apply_filters('wpuwooaskinvoice__text_ask_multiple', __('You already asked for an invoice', 'wpuwooaskinvoice'));
         $this->text_success_ask = apply_filters('wpuwooaskinvoice__text_success_ask', __('Invoice has been asked', 'wpuwooaskinvoice'));
         $this->text_email_title = apply_filters('wpuwooaskinvoice__text_email_title', __('Order #%s - Invoice', 'wpuwooaskinvoice'));
         $this->text_email_content = apply_filters('wpuwooaskinvoice__text_email_content', __('Please send the invoice for the order #%s', 'wpuwooaskinvoice'));
+        /* Values */
+        $this->authorized_statuses = apply_filters('wpuwooaskinvoice__authorized_statuses', $this->authorized_statuses);
+        $this->switch_meta_key = apply_filters('wpuwooaskinvoice__switch_meta_key', $this->switch_meta_key);
+        $this->enable_multiple_ask = apply_filters('wpuwooaskinvoice__enable_multiple_ask', $this->enable_multiple_ask);
     }
 
     public function template_redirect() {
-        global $post;
+        global $wp;
         if (!is_view_order_page() || empty($_POST) || !isset($_POST['wpuwooaskinvoice_ask'])) {
             return false;
         }
-        $wpuwooaskinvoice_asked = get_post_meta($post->ID, 'wpuwooaskinvoice_asked', 1);
-        if ($wpuwooaskinvoice_asked == 'ok') {
+        $order_id = $wp->query_vars['view-order'];
+        $wpuwooaskinvoice_asked = get_post_meta($order_id, $this->switch_meta_key, 1);
+        if ($wpuwooaskinvoice_asked == 'ok' && !$this->enable_multiple_ask) {
             return false;
         }
-        update_post_meta($post->ID, 'wpuwooaskinvoice_asked', 'ok');
+        update_post_meta($order_id, $this->switch_meta_key, 'ok');
         wc_add_notice($this->text_success_ask);
+        $order = wc_get_order($order_id);
+        $order->add_order_note($this->text_success_ask);
 
-        $text_email_title = sprintf($this->text_email_title, $post->ID);
-        $text_email_content = sprintf($this->text_email_content, $post->ID);
+        $text_email_title = sprintf($this->text_email_title, $order_id);
+        $text_email_content = sprintf($this->text_email_content, $order_id);
         wp_mail(get_option('admin_email'), $text_email_title, $text_email_content);
     }
 
     public function woocommerce_order_details_after_order_table($order) {
-        global $post;
-        if (!$order || $order->get_status() != 'on-hold') {
+        if (!$order || !in_array($order->get_status(), $this->authorized_statuses)) {
             return false;
         }
-        $wpuwooaskinvoice_asked = get_post_meta($post->ID, 'wpuwooaskinvoice_asked', 1);
-        if ($wpuwooaskinvoice_asked == 'ok') {
+        $wpuwooaskinvoice_asked = get_post_meta($order->get_id(), $this->switch_meta_key, 1);
+        if ($wpuwooaskinvoice_asked == 'ok' && !$this->enable_multiple_ask) {
             return false;
         }
 
+        echo '<div class="wpuwooaskinvoice-form-container">';
+        if ($wpuwooaskinvoice_asked == 'ok' && $this->enable_multiple_ask) {
+            echo '<p>' . $this->text_ask_multiple . '</p>';
+        }
         echo '<form class="wpuwooaskinvoice-form" action="" method="post"><p><button type="submit" name="wpuwooaskinvoice_ask">' . $this->text_ask . '</button></p></form>';
+        echo '</div>';
     }
 }
 

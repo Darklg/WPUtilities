93a9a8511d6e6579f3968febeee12db3ee021e75
Kevin <kevin@rocher-dev.com>
2017-09-24 23:56:26 +0200

    WPU MU Plugins Autoloader v 0.2
    * Display plugins in admin

--- wp-content/mu-plugins/wpu_muplugin_autoloader.php
+++ wp-content/mu-plugins/wpu_muplugin_autoloader.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU MU Plugins Autoloader
 Description: Load MU-Plugins in subfolders
-Version: 0.1
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -41,4 +41,43 @@ $wpu_muplugin_autoloader_list = wpu_muplugin_autoloader_list();
 foreach ($wpu_muplugin_autoloader_list as $wpu_muplugin_autoloader_plugin) {
     wp_register_plugin_realpath($wpu_muplugin_autoloader_plugin);
     require_once $wpu_muplugin_autoloader_plugin;
+}
+
+/* ----------------------------------------------------------
+  Display in admin
+---------------------------------------------------------- */
+
+add_action('show_advanced_plugins', 'wpu_muplugin_autoloader__show_advanced_plugins', 10, 2);
+function wpu_muplugin_autoloader__show_advanced_plugins($show, $type) {
+
+    /* Check correct page */
+    if (!is_admin()) {
+        return;
+    }
+    $screen = get_current_screen();
+    $current = is_multisite() ? 'plugins-network' : 'plugins';
+    if ($screen->base != $current || $type != 'mustuse' || !current_user_can('activate_plugins')) {
+        return $show;
+    }
+
+    /* Retrieve base plugins */
+    $mu_plugins = get_mu_plugins();
+
+    /* Add new plugins */
+    global $wpu_muplugin_autoloader_list;
+    foreach ($wpu_muplugin_autoloader_list as $plugin) {
+        $plugin_id = str_replace(WPMU_PLUGIN_DIR . '/', '', $plugin);
+        $plugin_id = str_replace('/', '___dir___', $plugin_id);
+        $mu_plugins[$plugin_id] = get_plugin_data($plugin, 0, 0);
+        if (!$mu_plugins[$plugin_id]['Name']) {
+            $mu_plugins[$plugin_id]['Name'] = basename($plugin);
+        }
+        $mu_plugins[$plugin_id]['Description'] = '(Autoloaded) ' . $mu_plugins[$plugin_id]['Description'];
+    }
+
+    /* Sort list */
+    uasort($mu_plugins, '_sort_uname_callback');
+
+    /* Override plugin list */
+    $GLOBALS['plugins']['mustuse'] = $mu_plugins;
 }

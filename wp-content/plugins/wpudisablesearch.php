ed0e2e7960765432150ade98cfb4c3dc34ee25c2
Kevin <kevin@rocher-dev.com>
2018-06-14 17:50:26 +0200

    WPU disable search v 0.2
    * Code fix for PHP 7+

--- wp-content/plugins/wpudisablesearch.php
+++ wp-content/plugins/wpudisablesearch.php
@@ -3,7 +3,7 @@
 Plugin Name: WPU disable search
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: Disable search
-Version: 0.1
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -11,20 +11,28 @@ License URI: http://opensource.org/licenses/MIT
 Thanks: http://www.geekpress.fr/?p=722
 */
 
-function wpu_disable_search_in_query( $query, $error = true ) {
-    if ( !is_search() ) {
+function wpu_disable_search__in_query($query, $error = true) {
+    if (!is_search()) {
         return;
     }
     $query->is_search = false;
     $query->query_vars['s'] = false;
     $query->query['s'] = false;
     // to error
-    if ( $error ){
+    if ($error) {
         $query->is_404 = true;
     }
 }
 
-if ( !is_admin() ) {
-    add_action( 'parse_query', 'wpu_disable_search_in_query' );
-    add_filter( 'get_search_form', create_function( '$a', "return null;" ) );
+function wpu_disable_search__get_search_form($a) {
+    return null;
+}
+
+add_action('init', 'wpu_disable_search__init');
+function wpu_disable_search__init() {
+    if (is_admin()) {
+        return;
+    }
+    add_action('parse_query', 'wpu_disable_search__in_query');
+    add_filter('get_search_form', 'wpu_disable_search__get_search_form');
 }

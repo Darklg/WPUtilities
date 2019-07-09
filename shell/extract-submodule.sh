8074dd30ad59b8fd7d2d03a60c2fa2a2003ce533
Kevin <kevin@rocher-dev.com>
2019-07-09 21:56:38 +0200

    Add a shell to extract a submodule to its own repository

---
+++ shell/extract-submodule.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+_PLUGIN='wpu_acf_flexible';
+_FILES="wp-content/mu-plugins/${_PLUGIN}.php";
+
+git clone 'https://github.com/darklg/wputilities' "${_PLUGIN}";
+cd "${_PLUGIN}";
+git remote remove origin;
+git filter-branch --prune-empty --index-filter "
+                        git read-tree --empty
+                        git reset \$GIT_COMMIT -- $_FILES
+                " \
+        -- --all -- $_FILES

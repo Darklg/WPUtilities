a72dbe7f614b5555bd2c513c8a5c7e61facfc288
Kevin <kevin@rocher-dev.com>
2020-05-21 17:39:22 +0200

    Merge WPUTheme-multiusers in WPUTheme + Remove Basic WPUTheme-child

--- wp-content/themes/WPUTheme-multiusers/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-include dirname( __FILE__ ) . '/../WPUTheme/z-protect.php';
-get_header();
-include get_stylesheet_directory() . '/tpl/account/user-toolbar.php';
-echo '<div class="main-content">';
-echo get_the_loop();
-echo '</div>';
-get_footer();
